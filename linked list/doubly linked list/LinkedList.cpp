f41e42f2400b3ee44e9898b87b97142476c59240
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-04-13 11:56:30 +0530

    Doubly linked list update

--- linked list/doubly linked list/LinkedList.cpp
+++ linked list/doubly linked list/LinkedList.cpp
@@ -27,27 +27,53 @@ class LinkedList{
             head = NULL;
         } 
 
-    void insertNode(int);
-    void deleteNode(int);
+    void insertAtEnd(int);
+    void insertAtBegin(int);
+    void deleteAtBegin();
+    void deleteAtEnd();
     void printList();
+
 };
 
-void LinkedList::insertNode(int data){
+void LinkedList::insertAtEnd(int data){
     Node *newNode = new Node(data);
     if(head == NULL){
         head = newNode;
         return;
     }
     Node *temp = head;
-    Node *prevNode = NULL;
     while(temp->next != NULL){
-        temp->prev = prevNode;
-        prevNode = temp; 
         temp = temp->next;
     }
     temp->next = newNode;
-    temp->prev = prevNode;
+    newNode->prev = temp;
 
+}
+
+void LinkedList::insertAtBegin(int data){
+    Node *newNode = new Node(data);
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+    return;
+}
+
+void LinkedList::deleteAtBegin(){
+    Node *newHead = head->next;
+    Node *temp = head;
+    newHead->prev = NULL;
+    head = newHead;
+    delete temp;
+}
+
+void LinkedList::deleteAtEnd(){
+    Node *temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    Node *prevNode = temp->prev;
+    prevNode->next = NULL;
+    delete temp;
 }
 
 void LinkedList::printList(){
@@ -60,12 +86,16 @@ void LinkedList::printList(){
 
 int main(){
     LinkedList list;
-    list.insertNode(10);
-    list.insertNode(20);
-    list.insertNode(30);
-    list.insertNode(40);
-    list.insertNode(50);
-    list.insertNode(60);
+    list.insertAtEnd(10);
+    list.insertAtEnd(20);
+    list.insertAtEnd(30);
+    list.insertAtEnd(40);
+    list.insertAtEnd(50);
+    list.insertAtEnd(60);
+    list.insertAtBegin(5);
+    list.insertAtBegin(4);
+    list.deleteAtBegin();
+    list.deleteAtEnd();
     list.printList();
     return 0;
 }
