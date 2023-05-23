8ee068a4ee0a01d2ea1a6ba3d6ab1ce62a0044f0
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-05-23 13:34:03 +0530

    bubble sort optimized way

--- sorting algo/bubbleSort.cpp
+++ sorting algo/bubbleSort.cpp
@@ -14,12 +14,30 @@ int main()
     }
     for (int i = 0; i < n - 1; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
             {
                 swap(arr[j], arr[j + 1]);
             }
+        }
+    }
+
+    // A more optimized method
+    for (int i = 0; i < n - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        if (swapped == true)
+        {
+            break;
         }
     }
     cout << "Sorting the array" << endl;
