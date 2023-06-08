743d47e94f1cae43fad72809ba3e50dfd16574bc
Abhishek-1337 <abhishekrinku4@gmail.com>
2023-06-08 20:33:31 +0530

    merge sort code implemented

---
+++ sorting algo/mergeSort.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// merge function
+void merge(int *arr, int low, int high, int mid)
+{ // merge function always takes three arguments l, m , r
+    int n1 = mid - low + 1, n2 = high - mid;
+    int left[n1], right[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        left[i] = arr[low + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        right[i] = arr[mid + 1 + i];
+    }
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] < right[j])
+        {
+            arr[k] = left[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = left[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        arr[k] = right[j];
+        j++;
+        k++;
+    }
+}
+
+// efficient
+void mergeSort(int *arr, int l, int r)
+{
+    if (l < r)
+    {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, r, m);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array\n";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the array to sort\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    mergeSort(arr, 0, n - 1);
+    cout << "After sorting the array\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    return 0;
+}
